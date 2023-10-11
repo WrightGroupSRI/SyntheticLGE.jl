6042d38db1d2fa5a11239e631b05cb3edc21d016
Calder Sheagren <caldersheagren@gmail.com>
2023-10-11 17:29:28 -0400

    integration testing [skip ci]

--- src/DataLoader.jl
+++ src/DataLoader.jl
@@ -15,11 +15,11 @@ using Glob
 using MIRTjim: jim
 
 """
-function DataLoader(multicontrast_path::String, T1_path::String)
+function DataLoader(multicontrast_path::String, T1_image_path::String)
 
 Arguments: 
 multicontrast_path: path to T1w data, sorted by contrast (i.e. full volume for contrast 1, full volume for contrast 2, ...) 
-T1_path: path to scanner-generated T1 maps
+T1_image_path: path to scanner-generated T1 maps
 
 Returns:
 [TI, T1w, T1map] (all from scanner images, no fitting done here)
@@ -27,13 +27,16 @@ Returns:
 function DataLoader(;multicontrast_path::String=nothing, T1_path::String=nothing)
 
 	@assert !isnothing(multicontrast_path) # need input
-	@assert !isnothing(T1_path) # need input
+	@assert !isnothing(T1_image_path) # need input
 
 	T1w_image_path = sort(glob("*.dcm", multicontrast_path))
-	T1_path = sort(glob("*.dcm", T1_path))
+	T1_image_path = sort(glob("*.dcm", T1_path))
+
+	@assert length(T1w_image_path) > 0 # Need data
+	@assert length(T1_image_path) > 0 # Need data
 
 	T1w_image_size = size(dcmread(T1w_image_path[1])[tag"Pixel Data"])
-	T1_map_size = size(dcmread(T1_path[1])[tag"Pixel Data"])
+	T1_map_size = size(dcmread(T1_image_path[1])[tag"Pixel Data"])
 	@assert T1w_image_size == T1_map_size # maps and images should be same size
 	shx, shy = T1w_image_size
 
@@ -51,7 +54,7 @@ function DataLoader(;multicontrast_path::String=nothing, T1_path::String=nothing
 
 	end
 
-	num_slices = size(T1_path, 1)
+	num_slices = size(T1_image_path, 1)
 
 	@assert mod(num_images, num_slices) == 0 # number of images should be a multiple of number of slices
 
@@ -80,7 +83,7 @@ function DataLoader(;multicontrast_path::String=nothing, T1_path::String=nothing
 	T1_map = zeros(shx, shy, num_slices)
 
 	for i = 1:num_slices
-		T1_map[:, :, i] = dcmread(T1_path[i])[tag"Pixel Data"] 
+		T1_map[:, :, i] = dcmread(T1_image_path[i])[tag"Pixel Data"] 
 	end
 
 	return TI_sort, T1w_sort, T1_map
