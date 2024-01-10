8d3ba411df8292295c424cce2f4afbbdb0f6cf90
Calder Sheagren <caldersheagren@gmail.com>
2024-01-10 14:33:35 -0500

    start denoising

--- src/SyntheticLGE.jl
+++ src/SyntheticLGE.jl
@@ -1,13 +1,14 @@
 module SyntheticLGE
 
-export IR, FitT1, dcmread, dcmwrite, DataLoader, CreateLGE, ExportLGE, norm
+export IR, FitT1, dcmread, dcmwrite, DataLoader, CreateLGE, ExportLGE, norm, α, wTV
 
+include("CreateLGE.jl")
 include("DataLoader.jl")
 include("DicomUtils.jl")
+include("ExportLGE.jl")
 include("FitT1.jl")
 include("IR.jl")
-include("CreateLGE.jl")
-include("ExportLGE.jl")
+include("MRIDenoise.jl")
 include("utils.jl")
 
 end
