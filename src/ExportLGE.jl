4428dc4e5a8dfc40e24e6e67da6ac9f7e1c07276
Calder Sheagren <caldersheagren@gmail.com>
2024-08-14 12:02:33 -0400

    update export to use nifti files

--- src/ExportLGE.jl
+++ src/ExportLGE.jl
@@ -11,6 +11,7 @@
 ###############################################################
 
 include("DicomUtils.jl")
+include("NIFTIUtils.jl")
 using Glob
 
 """
@@ -18,30 +19,12 @@ function ExportLGE()
 """
 function ExportLGE(LGE_array; reference_path=nothing, destination_path=nothing)
 
-	@assert !isnothing(destination_path) # need an input
-
-	@assert !isnothing(reference_path) # need an input
-
-	@assert isdir(reference_path) # reference path needs to exist
+	@assert !isnothing(destination_path) # need an output
 
 	if !isdir(destination_path)
 		mkpath(destination_path)
 	end
 
-	reference_files = sort(glob("*.dcm", reference_path))
-
-	for i=1:size(LGE_array, 3)
-
-		# get reference filename
-		img_reference_path = reference_files[i] 
-		
-		# create filename to save data
-		fnames = split(img_reference_path, "/")
-		img_destination_path = joinpath(destination_path, fnames[length(fnames)]) 
-		
-		# save data
-		dcmwrite(LGE_array[:, :, i], destination_path=img_destination_path, reference_path=img_reference_path)
-
-	end
+        niftiwrite(LGE_array, destination_path=destination_path)
 
 end
